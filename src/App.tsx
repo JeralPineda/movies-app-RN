f43eab4e36fb0b45cbb7a995013e796a77827f19
Jeral Pineda <jpineda@banhcafe.hn>
2023-08-03 10:38:24 -0600

    refactor: refactorizando componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,3 @@
-/**
- *API Now Playing
- * curl --request GET \
-     --url 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1' \
-     --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYjkzOGI3NzY4ZjdhNzEzODdjY2RjNGNmZGMyZGE1ZiIsInN1YiI6IjY0Y2FmN2FjZTA0ZDhhMDBlMzVlYTc3MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xd0EOaWVWD50fHj5LditQMcE3QFs2ew7Y3ZuLMjvyk4' \
-     --header 'accept: application/json'
- */
-
 import "react-native-gesture-handler";
 
 import React from "react";
