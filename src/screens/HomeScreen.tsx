e3dffbf839ab9144f65fd20bad6e92c95afecb4e
Jeral Pineda <jpineda@banhcafe.hn>
2023-08-03 10:57:52 -0600

    feat: agregando tipos para la data

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,14 @@
 import React, {useEffect} from "react";
 import {Text, View} from "react-native";
+
 import movieDB from "../api/movieDB";
+import {MovieDBNowPlaying} from "../interfaces/movie";
 
 export const HomeScreen = () => {
   useEffect(() => {
     (async () => {
-      const {data} = await movieDB.get("/now_playing");
-      console.log(JSON.stringify(data.results[0], null, 3));
+      const {data} = await movieDB.get<MovieDBNowPlaying>("/now_playing");
+      console.log(JSON.stringify(data.results[0].title, null, 3));
     })();
   }, []);
 
