98f347355a3f6040efc9020ca9998316473e013e
Jeral Pineda <jpineda@banhcafe.hn>
2023-08-04 15:17:35 -0600

    feat: creando card de actores

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,5 @@
 export {HorizontalSlider} from "./HorizontalSlider";
 export {MovieCarousel} from "./MovieCarousel";
+export {MovieDetails} from "./MovieDetails";
 export {MoviePoster} from "./MoviePoster";
 export * from "./ui";
