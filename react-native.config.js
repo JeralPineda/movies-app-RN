9113d1b93947a6e696a7aeaae55222eae46be551
Jeral Pineda <jpineda@banhcafe.hn>
2023-08-04 09:31:50 -0600

    feat: add fonts and configuration

---
+++ react-native.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  // dependencies: {
+  //   ...(process.env.NO_FLIPPER // or `` for RN@0.71.x and above
+  //     ? {"react-native-flipper": {platforms: {ios: null}}}
+  //     : {}),
+  // },
+  assets: ["./src/assets"],
+};
