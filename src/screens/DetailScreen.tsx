1c0aa94151cef67a7863a23615482f951272b58c
Jeral Pineda <jpineda@banhcafe.hn>
2023-08-04 12:49:11 -0600

    feat: diseño inicial de la pantalla de detalles

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,18 +1,70 @@
 import {StackScreenProps} from "@react-navigation/stack";
 import React from "react";
-import {Text, View} from "react-native";
+import {
+  Text,
+  View,
+  Image,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+} from "react-native";
 import {RootStackParams} from "../navigation/Navigation";
+
+const screenHeight = Dimensions.get("screen").height;
 
 interface Props extends StackScreenProps<RootStackParams, "DetailScreen"> {}
 
 export const DetailScreen = ({route}: Props) => {
   const movie = route.params;
-
-  console.log(movie.title);
+  const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
   return (
-    <View>
-      <Text>DetailScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.imageContainer}>
+        <Image source={{uri}} style={styles.posterImage as any} />
+      </View>
+
+      <View style={styles.detailContainer}>
+        <Text style={styles.subtitle}>{movie.original_title}</Text>
+        <Text style={styles.title}>{movie.title}</Text>
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    backgroundColor: "red",
+    overflow: "hidden",
+    width: "100%",
+    height: screenHeight * 0.7,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.24,
+    shadowRadius: 7,
+
+    elevation: 9,
+
+    borderBottomEndRadius: 25,
+    borderBottomStartRadius: 25,
+  },
+  posterImage: {
+    flex: 1,
+  },
+  detailContainer: {
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  subtitle: {
+    fontSize: 18,
+    opacity: 0.8,
+    fontFamily: "OpenSans-SemiBold",
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: "OpenSans-Bold",
+  },
+});
